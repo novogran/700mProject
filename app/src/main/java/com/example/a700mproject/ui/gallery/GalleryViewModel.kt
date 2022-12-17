9b40a3a6a0111dc10c7dc5ce1df223b1b026911f
Vitali Novogran <vi19954@gmail.com>
2022-12-17 16:36:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a700mproject/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.a700mproject.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
