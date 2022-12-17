403506857e42362123d053b53ee711e1eba96ecf
Vitali Novogran <vi19954@gmail.com>
2022-12-17 19:31:16 +0300

    set color for app, delete unnecessary elements and add russian localisation

--- app/src/main/java/com/example/a700mproject/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/a700mproject/ui/gallery/GalleryFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.a700mproject.databinding.FragmentGalleryBinding
+import com.example.a700mproject.databinding.FragmentTripsBinding
 
 class GalleryFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentTripsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,10 +25,10 @@ class GalleryFragment : Fragment() {
         val galleryViewModel =
             ViewModelProvider(this).get(GalleryViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentTripsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
+        val textView: TextView = binding.textTrips
         galleryViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
