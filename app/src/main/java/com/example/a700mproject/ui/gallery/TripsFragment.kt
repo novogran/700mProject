73e4f8dd5b969615590fb46f0a64cb3d0e24155c
Vitali Novogran <vi19954@gmail.com>
2022-12-21 21:46:23 +0300

    create UI of bonuses and trips fragments

--- app/src/main/java/com/example/a700mproject/ui/gallery/TripsFragment.kt
+++ app/src/main/java/com/example/a700mproject/ui/gallery/TripsFragment.kt
@@ -27,10 +27,7 @@ class TripsFragment : Fragment() {
 
         _binding = FragmentTripsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textTrips
         galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
         }
         return root
     }
--- app/src/main/java/com/example/a700mproject/ui/slideshow/BonusesFragment.kt
+++ app/src/main/java/com/example/a700mproject/ui/slideshow/BonusesFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.a700mproject.databinding.FragmentBonusesBinding
@@ -28,9 +27,7 @@ class BonusesFragment : Fragment() {
         _binding = FragmentBonusesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textBonuses
         bonusesViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
         }
         return root
     }
